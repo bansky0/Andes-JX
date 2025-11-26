e3c6c9b6dc9175f3f2cc5a9a619a4c1c8a9b4ab8
vnvillarrealv <136400980+vnvillarrealv@users.noreply.github.com>
2025-11-26 16:08:42 -0500

    Add files via upload

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -12,11 +12,11 @@
 //==============================================================================
 AndesJXAudioProcessorEditor::AndesJXAudioProcessorEditor (AndesJXAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
-{
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
-}
+    {
+        // Make sure that before the constructor has finished, you've set the
+        // editor's size to whatever you need it to be.
+        setSize (400, 300);
+    }
 
 AndesJXAudioProcessorEditor::~AndesJXAudioProcessorEditor()
 {
@@ -38,3 +38,4 @@ void AndesJXAudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
 }
+
